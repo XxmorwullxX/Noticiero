79ac4d55f107aff37d6548461a88a34f16f7df7c
Mor <alnetworking@hotmail.com>
2018-11-01 12:58:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,28 +3,44 @@ const client = new Discord.Client();
 
 client.login(process.env.BOT_TOKEN);
 
-let channelList = ['190818265923059712', '457922428883042315', '494757740292603904'];
-let channels = [];
+let channels = {};
+
+/*
+ * Aquí pones el tag y el canal que quieres que tenga el tag, me he inventado los valores
+ */
+const tags = {
+    PS: "457922428883042315"
+    PC: "190818265923059712", "457922428883042315", "494757740292603904"
+    NS: "458314272691191819"
+    TV: "244014666139828225", "457952200212348939", "395736059129888778", "294223645730144256"
+};
+
 
 client.on('ready', () => {
     console.log('Listo para dar las noticias');
 
-    for (const channel of channelList) {
-        channels.push(client.channels.get(channel));
+    for (const tag in tags) {
+        channels[tag] = client.channels.get(channel);
     }
 
     client.user.setGame('Noticias frescas')
 });
 
-client.on('message', (message) => {
 
+client.on('message', (message) => {
     if (message.author.id == 491885652846313473) {
         return;
     }
 
-    if (message.channel.type.toLowerCase() === 'dm' || message.channel.type.toLowerCase() === 'group') {
-        for (const channel of channels) {
-            channel.send(message.content);
+    for (const channel of channels) {
+        if (channel.type.toLowerCase() === 'dm' || channel.type.toLowerCase() === 'group') {
+            const splittedMessage = message.split(':', message.content);
+            const realMessage = splittedMessage[1];
+            const rawTagList = splittedMessage[0].split(',');
+
+            for (tag of rawTagList) {
+                channels[tag].send(realMessage);
+            }
         }
     }
 });
